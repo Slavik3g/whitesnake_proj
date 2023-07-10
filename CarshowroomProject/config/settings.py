fd477612677b15cb3cb0022a5e9035931f2d14d6
slavik <slavik3g@gmail.com>
2023-07-10 16:19:12 +0300

    add apps

--- CarshowroomProject/config/settings.py
+++ CarshowroomProject/config/settings.py
@@ -9,11 +9,13 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
+import sys
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -29,7 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 LOCAL_APPS = [
-
+    'apps.carshowroom',
+    'apps.customers',
+    'apps.suppliers',
 ]
 
 DJANGO_APPS = [
@@ -42,7 +46,7 @@ DJANGO_APPS = [
 ]
 
 THIRD_PARTIES_APPS = [
-
+    'rest_framework',
 ]
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTIES_APPS + LOCAL_APPS
 
