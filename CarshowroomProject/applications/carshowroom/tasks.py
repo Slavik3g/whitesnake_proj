61c02a7ef97b36611411593ae957e47b462093cf
slavik3g <slavik3g@gmail.com>
2023-07-24 11:07:01 +0300

    finish buy car from supplier

--- CarshowroomProject/applications/carshowroom/tasks.py
+++ CarshowroomProject/applications/carshowroom/tasks.py
@@ -1,3 +1,4 @@
+import decimal
 from _decimal import Decimal
 from datetime import datetime
 
@@ -8,6 +9,7 @@ from rest_framework.fields import DecimalField
 
 from applications.carshowroom.models import CarShowroomModel, CarShowroomCar, CarShowroomSupplierPurchaseHistory
 from applications.core.models import CarModel
+from applications.customers.models import CustomerPurchaseHistoryModel
 from applications.suppliers.models import SupplierCarModel, SupplierDiscount, SupplierModel
 
 
@@ -20,11 +22,16 @@ def max_percent_discount(car, supplier=None, carshowroom=None):
     return discounts if discounts else 0
 
 
-def max_count_for_bonus(supplier, carshowroom):
-    disc = CarShowroomSupplierPurchaseHistory.objects.filter(
-        carshowroom=carshowroom, supplier=supplier, is_active=True
-    ).aggregate(Sum('number_of_purchases'))['number_of_purchases__sum']
-    return disc if disc else 0
+def number_of_car_purchases(car, carshowroom):
+    return CustomerPurchaseHistoryModel.objects.filter(car=car, carshowroom=carshowroom).count()
+
+
+def get_individual_supplier_discount(supplier, carshowroom):
+    discount = CarShowroomSupplierPurchaseHistory.objects.filter(
+        carshowroom=carshowroom, supplier=supplier
+    ).aggregate(Sum('cars_count'))['cars_count__sum']
+    return discount if discount else 0
+
 
 @shared_task()
 def confirm_customers():
@@ -45,8 +52,39 @@ def confirm_customers():
                 )
 
 
-# @shared_task()
-# def buy_car_from_supplier():
-#     if car_showroom.balance >= supplier_car.price:
-#         car_showroom.balance -= supplier_car.price
-#         supplier_car.supplier.balance += supplier_car.price
+@shared_task()
+def buy_car_from_supplier():
+    carshowroomcars = CarShowroomCar.objects.all()
+    carshowroomcars = sorted(carshowroomcars,
+                             key=lambda x: number_of_car_purchases(car=carshowroomcars.car,
+                                                                   carshowroom=carshowroomcars.carshowroom),
+                             reverse=True)
+    for carshowroomcar in carshowroomcars:
+        car = carshowroomcar.car
+        supplier = carshowroomcar.supplier
+        carshowroom = carshowroomcar.carshowroom
+
+        supplier_car = SupplierCarModel.objects.filter(supplier=supplier, car=car).first()
+        if supplier_car:
+            individual_discount = get_individual_supplier_discount(carshowroom, supplier)
+            discount_price = supplier_car.price * decimal.Decimal(
+                (1 - (max_percent_discount(supplier=supplier, car=car) + individual_discount) / 100)
+            )
+            if carshowroom.balance >= discount_price:
+                carshowroom.balance -= Decimal(discount_price)
+                supplier.balance += Decimal(discount_price)
+                purchase_history = CarShowroomSupplierPurchaseHistory.objects.get_or_create(
+                    car=car,
+                    supplier=supplier,
+                    carshowroom=carshowroom
+                )
+                purchase_history.cars_count += 1
+                purchase_history.total_price += Decimal(discount_price)
+                carshowroomcar.number += 1
+
+                carshowroom.save()
+                supplier.save()
+                purchase_history.save()
+                carshowroomcar.save()
+
+
