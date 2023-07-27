f3c39df9c5942a26cd514a3b63ccd852c7b0a109
slavik3g <slavik3g@gmail.com>
2023-07-27 23:22:32 +0300

    moved send mail to celery

---
+++ CarshowroomProject/applications/core/tasks.py
@@ -0,0 +1,17 @@
+from celery import shared_task
+from django.core.mail import send_mail
+
+from applications.core.services import UserService
+
+user_service = UserService()
+
+
+@shared_task
+def send_email_task(subject: str, message: str, recipient_list: list, from_email='noreply@example.com'):
+    send_mail(
+        subject,
+        message,
+        from_email,
+        recipient_list,
+        fail_silently=False,
+    )
